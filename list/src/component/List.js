0da63e959f8cd250d26bfebc08c9b6cafa2651a8
mjsvijit <viratsinghfzd75@gmail.com>
2022-02-16 17:51:08 +0530

    Create List.js

---
+++ list/src/component/List.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const List = () => {
+  return (
+    <div>
+     
+        <h1>Mobile Operating System</h1>
+             <ul>
+                 <li>Andriod</li>
+                 <li>Black Berray</li>
+                 <li>iphone</li>
+                 <li>Windows Phone</li>
+             </ul>
+        <h1>Mobile Manufacturers</h1>
+             <ul>
+                <li>Samsung</li>
+                <li>HTC</li>
+                <li>Micromax</li>
+                <li>Apple</li>
+             
+             </ul>
+    
+   
+        
+    </div>
+  )
+}
