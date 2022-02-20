4300626b56978869be96f68bcf1c34f82d0efe71
mjsvijit <viratsinghfzd75@gmail.com>
2022-02-20 22:18:32 +0530

    Create App.js

---
+++ destructuring/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Getval } from './Getval';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Getval name="Vijit" surname="Singh"/>
+    </div>
+  );
+}
+
+export default App;
