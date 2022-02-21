d055a39a0e40a71d3da54a258bfc2d20b6e20db8
mjsvijit <viratsinghfzd75@gmail.com>
2022-02-21 21:52:55 +0530

    Create App.js

---
+++ loginpage/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BascForm } from './form/bascForm';
+
+function App() {
+  return (
+    <div className="App">
+     <BascForm/>
+    </div>
+  );
+}
+
+export default App;
